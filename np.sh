0310c9cf2a98280af8edeef0a65d327f19fbec66
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2015-11-11 09:37:26 -0800

    Fix for https://github.com/sindresorhus/np/issues/2

--- np.sh
+++ np.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env sh
+
+if test 0 -ne `git status --porcelain | wc -l`; then
+	echo "Unclean working tree. Commit or stash changes first." >&2;
+	exit 128;
+fi
+
+if test "00" -ne `git fetch; git rev-list --count --left-right "@{upstream}"...HEAD | awk '{ print $1$2 }'`; then
+	echo "Local/Remote history differs. Please push/pull changes." >&2;
+	exit 128;
+fi
+
 ./node_modules/.bin/trash node_modules &>/dev/null;
-git pull --rebase &&
 npm install &&
 npm test &&
 npm version ${1:-patch} &&
