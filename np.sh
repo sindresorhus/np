bfc17815b9e4a59d654401aae114a49c457d2541
Mathias Fredriksson <mathias@walkbase.com>
2015-11-23 17:01:38 +0700

    Close #8 PR: Only check remote for changes, allow pushing local changes.

--- np.sh
+++ np.sh
@@ -10,8 +10,8 @@ if ! git fetch --quiet 2>/dev/null; then
 	exit 128;
 fi
 
-if test "00" != "$(git rev-list --count --left-right @'{u}'...HEAD | awk '{ print $1$2 }')"; then
-	echo 'Local/remote history differ. Please push/pull changes.' >&2;
+if test "0" != "$(git rev-list --count --left-only @'{u}'...HEAD)"; then
+	echo 'Remote history differs, please pull changes.' >&2;
 	exit 128;
 fi
 
