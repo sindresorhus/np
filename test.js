a46301d12a498be4e34467e5a48fc4b1a14f7c31
Scott Hardy <scott.the.hardy@gmail.com>
2016-08-10 19:14:58 -0700

    Added test for pre-release check

--- test.js
+++ test.js
@@ -1,7 +1,18 @@
 import test from 'ava';
 import m from './';
 
-test('wrong input', t => {
-	t.throws(m('foo'), 'Version should be either major, minor, patch, premajor, preminor, prepatch, prerelease, or a valid semver version.');
-	t.throws(m('4.x.3'), 'Version should be either major, minor, patch, premajor, preminor, prepatch, prerelease, or a valid semver version.');
+test('version is invalud', t => {
+	const message = 'Version should be either major, minor, patch, premajor, preminor, prepatch, prerelease, or a valid semver version.';
+	t.throws(m('foo'), message);
+	t.throws(m('4.x.3'), message);
+});
+
+test('version is pre-release', t => {
+	const message = 'You must specify a dist-tag using --tag when publishing a pre-release version. This prevents accidentally tagging unstable versions as "latest".';
+	t.throws(m('premajor'), message);
+	t.throws(m('preminor'), message);
+	t.throws(m('prepatch'), message);
+	t.throws(m('prerelease'), message);
+	t.throws(m('1.0.0-0'), message);
+	t.throws(m('1.0.0-beta'), message);
 });
