4bb1f5d5cce81410ca3ef4cee4d01acdf62559ab
Scott Hardy <scott.the.hardy@gmail.com>
2016-08-11 12:33:49 -0700

    Updated tests with new pre-release check error message

--- test.js
+++ test.js
@@ -8,7 +8,7 @@ test('version is invalid', t => {
 });
 
 test('version is pre-release', t => {
-	const message = 'You must specify a dist-tag using --tag when publishing a pre-release version. This prevents accidentally tagging unstable versions as "latest".';
+	const message = 'You must specify a dist-tag using --tag when publishing a pre-release version. This prevents accidentally tagging unstable versions as "latest". https://docs.npmjs.com/cli/dist-tag';
 	t.throws(m('premajor'), message);
 	t.throws(m('preminor'), message);
 	t.throws(m('prepatch'), message);
