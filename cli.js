ca0f193170618547a56c130cddc8bfd86e3c8f00
Dieter Luypaert <dieterluypaert@gmail.com>
2016-12-13 14:27:42 +0100

    Add `--no-publish` flag (#113)

--- cli.js
+++ cli.js
@@ -18,6 +18,7 @@ const cli = meow(`
 	  --any-branch  Allow publishing from any branch
 	  --no-cleanup  Skips cleanup of node_modules
 	  --yolo        Skips cleanup and testing
+	  --no-publish  Skips publishing
 	  --tag         Publish under a given dist-tag
 
 	Examples
