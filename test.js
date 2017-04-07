3cddcdedb7319016666b16b9ec0d353763373a4d
Sam Verschueren <sam.verschueren@gmail.com>
2017-04-07 20:30:18 +0200

    Fix tests

--- test.js
+++ test.js
@@ -1,23 +1,23 @@
 import test from 'ava';
 import m from './';
 
-test('version is invalid', t => {
+test('version is invalid', async t => {
 	const message = 'Version should be either patch, minor, major, prepatch, preminor, premajor, prerelease, or a valid semver version.';
-	t.throws(m('foo'), message);
-	t.throws(m('4.x.3'), message);
+	await t.throws(m('foo'), message);
+	await t.throws(m('4.x.3'), message);
 });
 
-test('version is pre-release', t => {
+test('version is pre-release', async t => {
 	const message = 'You must specify a dist-tag using --tag when publishing a pre-release version. This prevents accidentally tagging unstable versions as "latest". https://docs.npmjs.com/cli/dist-tag';
-	t.throws(m('premajor'), message);
-	t.throws(m('preminor'), message);
-	t.throws(m('prepatch'), message);
-	t.throws(m('prerelease'), message);
-	t.throws(m('10.0.0-0'), message);
-	t.throws(m('10.0.0-beta'), message);
+	await t.throws(m('premajor'), message);
+	await t.throws(m('preminor'), message);
+	await t.throws(m('prepatch'), message);
+	await t.throws(m('prerelease'), message);
+	await t.throws(m('10.0.0-0'), message);
+	await t.throws(m('10.0.0-beta'), message);
 });
 
-test('errors on too low version', t => {
-	t.throws(m('1.0.0'), /New version `1\.0\.0` should be higher than current version `\d+\.\d+\.\d+`/);
-	t.throws(m('1.0.0-beta'), /New version `1\.0\.0-beta` should be higher than current version `\d+\.\d+\.\d+`/);
+test('errors on too low version', async t => {
+	await t.throws(m('1.0.0'), /New version `1\.0\.0` should be higher than current version `\d+\.\d+\.\d+`/);
+	await t.throws(m('1.0.0-beta'), /New version `1\.0\.0-beta` should be higher than current version `\d+\.\d+\.\d+`/);
 });
