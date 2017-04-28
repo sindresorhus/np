96c672ce6ec8bafced6cd342b430033c6da176be
Greg Poole <greg.poole@domain.com.au>
2017-04-28 17:10:16 +1000

    Wrong indentation type!

--- index.js
+++ index.js
@@ -68,11 +68,11 @@ module.exports = (input, opts) => {
 				title: 'Installing dependencies using Yarn',
 				enabled: () => opts.yarn === true,
 				task: () => exec('yarn', ['install', '--frozen-lockfile']).catch((err) => {
-          if (err.stderr.startsWith('error Your lockfile needs to be updated')) {
-            throw new Error('yarn.lock file is outdated. Run yarn, commit the updated lockfile and try again.');
-          }
-          throw err;
-        })
+					if (err.stderr.startsWith('error Your lockfile needs to be updated')) {
+						throw new Error('yarn.lock file is outdated. Run yarn, commit the updated lockfile and try again.');
+					}
+					throw err;
+				})
 			},
 			{
 				title: 'Installing dependencies using npm',
