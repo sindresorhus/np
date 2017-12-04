sindresorhus/np

commit 78200bc4f2800e00074b5c972978b858e69583e9

Bump dependencies

cli.js
@@ -27,7 +27,28 @@ const cli = meow(`
 	  $ np patch
 	  $ np 1.0.2
 	  $ np 1.0.2-beta.3 --tag=beta
-`);
+`, {
+	flags: {
+		anyBranch: {
+			type: 'boolean'
+		},
+		cleanup: {
+			type: 'boolean'
+		},
+		yolo: {
+			type: 'boolean'
+		},
+		publish: {
+			type: 'boolean'
+		},
+		tag: {
+			type: 'string'
+		},
+		yarn: {
+			type: 'boolean'
+		}
+	}
+});
 
 updateNotifier({pkg: cli.pkg}).notify();
 

commit 04beee49950d94e4a055a0dc8b6440205120537a

Properly set default flag values
The issue was introduced in 78200bc4f2800e00074b5c972978b858e69583e9

Fixes #208

cli.js
@@ -33,13 +33,15 @@ const cli = meow(`
 			type: 'boolean'
 		},
 		cleanup: {
-			type: 'boolean'
+			type: 'boolean',
+			default: true
 		},
 		yolo: {
 			type: 'boolean'
 		},
 		publish: {
-			type: 'boolean'
+			type: 'boolean',
+			default: true
 		},
 		tag: {
 			type: 'string'