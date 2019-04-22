5acfb70c604f60786a1f82dbdfab3cda8815090e
Jonathan Cardoso Machado <me@jonathancardoso.com>
2019-04-22 09:26:13 -0300

    Skip deleting `node_modules` if `yarn.lock` exists (#392)

--- source/index.js
+++ source/index.js
@@ -57,7 +57,7 @@ module.exports = async (input = 'patch', options) => {
 	const pkgManager = options.yarn === true ? 'yarn' : 'npm';
 	const pkgManagerName = options.yarn === true ? 'Yarn' : 'npm';
 	const rootDir = pkgDir.sync();
-	const hasLockFile = fs.existsSync(path.resolve(rootDir, 'package-lock.json')) || fs.existsSync(path.resolve(rootDir, 'npm-shrinkwrap.json'));
+	const hasLockFile = fs.existsSync(path.resolve(rootDir, options.yarn ? 'yarn.lock' : 'package-lock.json')) || fs.existsSync(path.resolve(rootDir, 'npm-shrinkwrap.json'));
 	const isOnGitHub = options.repoUrl && hostedGitInfo.fromUrl(options.repoUrl).type === 'github';
 
 	let isPublished = false;
