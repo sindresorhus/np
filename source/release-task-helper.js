4089011fcb23c2737954206adc525b5a10bf489d
Yao Ding <yao.ding@compass.com>
2019-06-22 21:56:42 -0400

    fix release draft uses wrong tag

--- source/release-task-helper.js
+++ source/release-task-helper.js
@@ -1,11 +1,12 @@
 'use strict';
 const open = require('open');
-const newGithubReleaseUrl = require('./new-github-release-url');
+const newGithubReleaseUrl = require('new-github-release-url');
 const {getTagVersionPrefix} = require('./util');
 const version = require('./version');
 
-module.exports = async options => {
-	const tag = await getTagVersionPrefix(options) + options.version;
+module.exports = async (options, pkg) => {
+	const newVersion = version(pkg.version).getNewVersionFrom(options.version);
+	const tag = await getTagVersionPrefix(options) + newVersion;
 	const url = newGithubReleaseUrl({
 		repoUrl: options.repoUrl,
 		tag,
