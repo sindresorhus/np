589dc8fd28d366a052f0c1f7150e2806f9e3df10
Charles Kenney <Charlesc.kenney@gmail.com>
2019-06-30 03:46:24 -0400

    Increase util.js test coverage (#419)

--- test/prefix.js
+++ test/prefix.js
@@ -1,4 +1,5 @@
 import test from 'ava';
+import proxyquire from 'proxyquire';
 import {getTagVersionPrefix} from '../source/util';
 
 test('get tag prefix', async t => {
@@ -9,4 +10,9 @@ test('get tag prefix', async t => {
 test('no options passed', async t => {
 	await t.throwsAsync(getTagVersionPrefix(), {message: 'Expected `options` to be of type `object` but received type `undefined`'});
 	await t.throwsAsync(getTagVersionPrefix({}), {message: 'Expected object `options` to have keys `["yarn"]`'});
+});
+
+test.serial('defaults to "v" when command fails', async t => {
+	proxyquire('../source/util', {execa: Promise.reject});
+	t.is(await getTagVersionPrefix({yarn: true}), 'v');
 });
