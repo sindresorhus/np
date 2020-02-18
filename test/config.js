5a430420a2586eac9364affb20e0b4241d62bfe4
Bunyanuch Saengnet <bunyanuch.saengnet@kabelsurfer.com>
2020-02-18 19:23:47 +0000

    Added edge cases for config test

--- test/config.js
+++ test/config.js
@@ -4,27 +4,31 @@ import test from 'ava';
 import sinon from 'sinon';
 import proxyquire from 'proxyquire';
 
-const stubOs = sinon.stub(os, 'homedir');
+const homedirStub = sinon.stub(os, 'homedir');
 
-test('should return config from `.np-config.json` when installed globally', async t => {
-	stubOs.returns(path.resolve('test', 'fixtures', 'homedir1'));
+test('should return config from `.np-config.json` in home-directory when global binary used', async t => {
+	homedirStub.returns(path.resolve('test', 'fixtures', 'homedir1'));
 	const config = proxyquire('../source/config', {
 		'is-installed-globally': true,
-		'pkg-dir': async () => {}
+		'pkg-dir': async () => {
+			return path.resolve('test', 'fixtures', 'pkg-dir');
+		}
 	});
 	t.deepEqual(await config(), {yarn: false});
 });
 
-test('should return config from `.np-config.js` when installed globally', async t => {
-	stubOs.returns(path.resolve('test', 'fixtures', 'homedir2'));
+test('should return config from `.np-config.js` in home-directory when global binary used', async t => {
+	homedirStub.returns(path.resolve('test', 'fixtures', 'homedir2'));
 	const config = proxyquire('../source/config', {
 		'is-installed-globally': true,
-		'pkg-dir': async () => {}
+		'pkg-dir': async () => {
+			return path.resolve('test', 'fixtures', 'pkg-dir');
+		}
 	});
 	t.deepEqual(await config(), {yarn: true, contents: 'dist'});
 });
 
-test('should return config from `package.json` when installed localy', async t => {
+test('should return config from `package.json` when local binary used', async t => {
 	const config = proxyquire('../source/config', {
 		'is-installed-globally': false,
 		'pkg-dir': async () => {
@@ -32,4 +36,49 @@ test('should return config from `package.json` when installed localy', async t =
 		}
 	});
 	t.deepEqual(await config(), {yarn: true});
+});
+
+test('should only return config from home-directory when global binary used', async t => {
+	homedirStub.returns(path.resolve('test', 'fixtures', 'homedir1'));
+	const globalConfig = proxyquire('../source/config', {
+		'is-installed-globally': true,
+		'pkg-dir': async () => {
+			throw new Error('access local config');
+		}
+	});
+	const localConfig = proxyquire('../source/config', {
+		'is-installed-globally': false,
+		'pkg-dir': async () => {
+			throw new Error('expected');
+		}
+	});
+	t.deepEqual(await globalConfig(), {yarn: false});
+	await t.throwsAsync(localConfig());
+});
+
+test('should only return config from local package when local binary used', async t => {
+	const globalConfig = proxyquire('../source/config', {
+		'is-installed-globally': true,
+		'pkg-dir': async () => {
+			return path.resolve('test', 'fixtures', 'local_config');
+		},
+		os: {
+			homedir: () => {
+				throw new Error('expected');
+			}
+		}
+	});
+	const localConfig = proxyquire('../source/config', {
+		'is-installed-globally': false,
+		'pkg-dir': async () => {
+			return path.resolve('test', 'fixtures', 'local_config');
+		},
+		os: {
+			homedir: () => {
+				throw new Error('access global config in home-directory');
+			}
+		}
+	});
+	await t.throwsAsync(globalConfig());
+	t.deepEqual(await localConfig(), {local: true});
 });
