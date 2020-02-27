786187d7fd1bac17d7cee047db3dc1293921aeea
Bunyanuch Saengnet <bunyanuch.saengnet@kabelsurfer.com>
2020-02-27 21:15:13 +0000

    Show files added since last release refactoring testclass

--- test/npmignore.js
+++ test/npmignore.js
@@ -13,8 +13,6 @@ const pkgDirApi = {
 	sync: () => {}
 };
 
-const stubPkgDir = sinon.stub(pkgDirApi, 'sync');
-
 test.before(() => {
 	const stubGitUtil = sinon.stub(gitUtilApi, 'newFilesSinceLastRelease');
 
@@ -31,6 +29,7 @@ test.before(() => {
 		warnOnUnregistered: false
 	});
 
+	// Mockery has to setup before module is loaded
 	testedModule = require('../source/util');
 });
 
@@ -40,30 +39,40 @@ test.after(() => {
 });
 
 test.serial('ignored files using file-attribute in package.json with one file', async t => {
+	const stubPkgDir = sinon.stub();
 	stubPkgDir.returns(path.resolve('test', 'fixtures', 'package'));
+	pkgDirApi.sync = stubPkgDir;
 	t.deepEqual(await testedModule.getNewAndUnpublishedFiles({files: ['pay_attention.txt']}),
 		['source/ignore.txt', 'test/file.txt']);
 });
 
 test.serial('ignored files using file-attribute in package.json with multiple file', async t => {
+	const stubPkgDir = sinon.stub();
 	stubPkgDir.returns(path.resolve('test', 'fixtures', 'package'));
+	pkgDirApi.sync = stubPkgDir;
 	t.deepEqual(await testedModule.getNewAndUnpublishedFiles(
 		{files: ['pay_attention.txt', 'ignore.txt']}), ['test/file.txt']);
 });
 
 test.serial('ignored file using file-attribute in package.json with directory', async t => {
+	const stubPkgDir = sinon.stub();
 	stubPkgDir.returns(path.resolve('test', 'fixtures', 'package'));
+	pkgDirApi.sync = stubPkgDir;
 	t.deepEqual(await testedModule.getNewAndUnpublishedFiles(
 		{files: ['source']}), ['test/file.txt']);
 });
 
 test.serial('ignored files using .npmignore', async t => {
+	const stubPkgDir = sinon.stub();
 	stubPkgDir.returns(path.resolve('test', 'fixtures', 'npmignore'));
+	pkgDirApi.sync = stubPkgDir;
 	t.deepEqual(await testedModule.getNewAndUnpublishedFiles({name: 'without file-attribute'}),
 		['source/ignore.txt', 'test/file.txt']);
 });
 
 test.serial('ignore strategy is not used', async t => {
+	const stubPkgDir = sinon.stub();
 	stubPkgDir.returns(path.resolve('test', 'fixtures'));
+	pkgDirApi.sync = stubPkgDir;
 	t.true(await testedModule.getNewAndUnpublishedFiles({name: 'without file-attribute'}) === undefined);
 });
