be255f986c5e56f1628ac5df7a8c93c252d34109
Bunyanuch Saengnet <53788417+bunysae@users.noreply.github.com>
2020-04-22 10:01:23 +0000

    Update source/ui.js
    Co-Authored-By: Sindre Sorhus <sindresorhus@gmail.com>

--- source/ui.js
+++ source/ui.js
@@ -139,7 +139,7 @@ module.exports = async (options, pkg) => {
 		{
 			type: 'confirm',
 			name: 'publishScoped',
-			when: isScoped(pkg.name) && !options.availability.isAvailable && !options.availability.isUnknown && options.runPublish && (pkg.publishConfig && pkg.publishConfig.access !== 'restricted') && !isExternalRegistry(pkg),
+			when: isScoped(pkg.name) && options.availability.isAvailable && !options.availability.isUnknown && options.runPublish && (pkg.publishConfig && pkg.publishConfig.access !== 'restricted') && !isExternalRegistry(pkg),
 			message: `This scoped repo ${chalk.bold.magenta(pkg.name)} hasn't been published. Do you want to publish it publicly?`,
 			default: false
 		}
