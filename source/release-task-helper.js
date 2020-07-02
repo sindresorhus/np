e55969b9fa6f5d3ec18efe3ab9aecc2b9616d23c
Govind <gvind4@gmail.com>
2020-07-02 18:44:36 +0530

    Add PreId feature to np

--- source/release-task-helper.js
+++ source/release-task-helper.js
@@ -1,12 +1,12 @@
 'use strict';
 const open = require('open');
 const newGithubReleaseUrl = require('new-github-release-url');
-const {getTagVersionPrefix} = require('./util');
+const {getTagVersionPrefix, getPreId} = require('./util');
 const version = require('./version');
 
 module.exports = async (options, pkg) => {
 	const newVersion = version(pkg.version).getNewVersionFrom(options.version);
-	const tag = await getTagVersionPrefix(options) + newVersion;
+	const tag = await getTagVersionPrefix(options) + newVersion  + await getPreId(options);
 
 	const url = newGithubReleaseUrl({
 		repoUrl: options.repoUrl,
--- source/util.js
+++ source/util.js
@@ -68,3 +68,25 @@ exports.getTagVersionPrefix = pMemoize(async options => {
 		return 'v';
 	}
 });
+
+exports.getPreId = pMemoize(async options => {
+    ow(options, ow.object.hasKeys('yarn'));
+
+    try {
+        if (options.yarn) {
+            const { stdout } = await execa('yarn', ['config', 'get', 'preId']);
+            if (stdout !== 'undefined') {
+                return stdout;
+            }
+            return '';
+        }
+
+        const { stdout } = await execa('npm', ['config', 'get', 'preId']);
+        if (stdout !== 'undefined') {
+            return stdout;
+        }
+        return '';
+    } catch (_) {
+        return '';
+    }
+})
