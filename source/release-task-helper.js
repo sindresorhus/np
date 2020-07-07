52ce84b2e808759ada13c964c7bde13dc1aba96c
nimish <nimish@sno.global>
2020-07-07 20:29:29 +0530

    fixing xo error

--- source/release-task-helper.js
+++ source/release-task-helper.js
@@ -5,7 +5,6 @@ const {getTagVersionPrefix, getPreReleasePrefix} = require('./util');
 const version = require('./version');
 
 module.exports = async (options, pkg) => {
-
 	const newVersion = version(pkg.version).getNewVersionFrom(options.version);
 	let tag = await getTagVersionPrefix(options) + newVersion;
 	const isPreRelease = version(options.version).isPrerelease();
