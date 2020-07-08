71d410f9be78f1d8a9a0ca683613f1e1f253adb6
Bunyanuch Saengnet <bunyanuch.saengnet@kabelsurfer.com>
2020-07-08 14:01:21 +0000

    Simplify tests

--- test/npmignore.js
+++ test/npmignore.js
@@ -1,78 +1,46 @@
 import path from 'path';
 import test from 'ava';
-import mockery from 'mockery';
-import sinon from 'sinon';
+import proxyquire from 'proxyquire';
 
-let testedModule;
+const newFiles = ['source/ignore.txt', 'source/pay_attention.txt', '.hg', 'test/file.txt'];
 
-const gitUtilApi = {
-	newFilesSinceLastRelease: async () => {}
-};
-
-const pkgDirApi = {
-	sync: () => {}
-};
-
-test.before(() => {
-	const stubGitUtil = sinon.stub(gitUtilApi, 'newFilesSinceLastRelease');
-
-	mockery.registerAllowable('../source/util');
-	mockery.registerAllowable('../source/npm/util');
-	mockery.registerMock('./git-util', gitUtilApi);
-	mockery.registerMock('pkg-dir', pkgDirApi);
-
-	stubGitUtil.returns(['source/ignore.txt', 'source/pay_attention.txt', '.hg', 'test/file.txt']);
-
-	mockery.enable({
-		useCleanCache: true,
-		warnOnReplace: false,
-		warnOnUnregistered: false
+test('ignored files using file-attribute in package.json with one file', async t => {
+	const testedModule = proxyquire('../source/npm/util', {
+		'pkg-dir':
+			{
+				sync: () => path.resolve('test', 'fixtures', 'package')
+			}
 	});
-
-	// Mockery has to setup before module is loaded
-	testedModule = require('../source/util');
-});
-
-test.after(() => {
-	mockery.deregisterAll();
-	mockery.disable();
-});
-
-test.serial('ignored files using file-attribute in package.json with one file', async t => {
-	const stubPkgDir = sinon.stub();
-	stubPkgDir.returns(path.resolve('test', 'fixtures', 'package'));
-	pkgDirApi.sync = stubPkgDir;
-	t.deepEqual(await testedModule.getNewAndUnpublishedFiles({files: ['pay_attention.txt']}),
+	t.deepEqual(await testedModule.getNewAndUnpublishedFiles(['pay_attention.txt'], newFiles),
 		['source/ignore.txt', 'test/file.txt']);
 });
 
-test.serial('ignored files using file-attribute in package.json with multiple file', async t => {
-	const stubPkgDir = sinon.stub();
-	stubPkgDir.returns(path.resolve('test', 'fixtures', 'package'));
-	pkgDirApi.sync = stubPkgDir;
-	t.deepEqual(await testedModule.getNewAndUnpublishedFiles(
-		{files: ['pay_attention.txt', 'ignore.txt']}), ['test/file.txt']);
+test('ignored file using file-attribute in package.json with directory', async t => {
+	const testedModule = proxyquire('../source/npm/util', {
+		'pkg-dir':
+			{
+				sync: () => path.resolve('test', 'fixtures', 'package')
+			}
+	});
+	t.deepEqual(await testedModule.getNewAndUnpublishedFiles(['source'], newFiles), ['test/file.txt']);
 });
 
-test.serial('ignored file using file-attribute in package.json with directory', async t => {
-	const stubPkgDir = sinon.stub();
-	stubPkgDir.returns(path.resolve('test', 'fixtures', 'package'));
-	pkgDirApi.sync = stubPkgDir;
-	t.deepEqual(await testedModule.getNewAndUnpublishedFiles(
-		{files: ['source']}), ['test/file.txt']);
+test('ignored files using .npmignore', async t => {
+	const testedModule = proxyquire('../source/npm/util', {
+		'pkg-dir':
+			{
+				sync: () => path.resolve('test', 'fixtures', 'npmignore')
+			}
+	});
+	t.deepEqual(await testedModule.getNewAndUnpublishedFiles(undefined, newFiles), ['source/ignore.txt', 'test/file.txt']);
 });
 
-test.serial('ignored files using .npmignore', async t => {
-	const stubPkgDir = sinon.stub();
-	stubPkgDir.returns(path.resolve('test', 'fixtures', 'npmignore'));
-	pkgDirApi.sync = stubPkgDir;
-	t.deepEqual(await testedModule.getNewAndUnpublishedFiles({name: 'without file-attribute'}),
-		['source/ignore.txt', 'test/file.txt']);
-});
-
-test.serial('ignore strategy is not used', async t => {
-	const stubPkgDir = sinon.stub();
-	stubPkgDir.returns(path.resolve('test', 'fixtures'));
-	pkgDirApi.sync = stubPkgDir;
-	t.true(await testedModule.getNewAndUnpublishedFiles({name: 'without file-attribute'}) === undefined);
+test('ignore strategy is not used', async t => {
+	const testedModule = proxyquire('../source/npm/util', {
+		'pkg-dir':
+			{
+				sync: () => path.resolve('test', 'fixtures')
+			}
+	});
+	t.true(await testedModule.getNewAndUnpublishedFiles(undefined, newFiles) === undefined);
 });
