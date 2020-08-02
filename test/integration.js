bd12ee8da9e373b31a535c5b9bfba374ff010f72
Bunyanuch Saengnet <bunyanuch.saengnet@kabelsurfer.com>
2020-08-02 16:34:34 +0000

    integration-test: cleanup task

--- test/integration.js
+++ test/integration.js
@@ -5,6 +5,10 @@ test.before(async () => {
 	await execa('npm', ['i'], {cwd: 'integration-test'});
 });
 
+test.after.always(async () => {
+	await execa('git', ['submodule', 'update', '--remote']);
+});
+
 test('Integration tests', async t => {
 	await execa('ava', {cwd: 'integration-test'});
 	t.pass();
