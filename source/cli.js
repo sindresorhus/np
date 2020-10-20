9c04fcbdbd4bb2cd6a05056518a6dbbf20d34173
James Talmage <james@talmage.io>
2020-10-19 22:54:21 -0400

    use debuglog

--- source/cli.js
+++ source/cli.js
@@ -1,12 +1,15 @@
 #!/usr/bin/env node
 'use strict';
+const util = require('util');
 const importLocal = require('import-local');
 const isInstalledGlobally = require('is-installed-globally');
+
+const debuglog = util.debuglog('np');
 
 // Prefer the local installation
 if (!importLocal(__filename)) {
 	if (isInstalledGlobally) {
-		console.log('Using global install of np.');
+		debuglog('Using global install of np.');
 	}
 
 	// eslint-disable-next-line import/no-unassigned-import
