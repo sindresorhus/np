3cd457d7ab5665ec013fbb059578b1f82ec6e87a
Bunyanuch Saengnet <bunyanuch.saengnet@kabelsurfer.com>
2020-11-28 09:03:04 +0000

    run integration test with github actions

--- test/integration.js
+++ test/integration.js
@@ -1,11 +1,20 @@
 const test = require('ava');
 const execa = require('execa');
 
+test.before(async () => {
+	if (process.env.GITHUB_ACTIONS) {
+		console.log('running on Github actions');
+		await execa('git', ['config', '--global', 'user.name', 'Github actions']);
+		await execa('git', ['config', '--global', 'user.email', 'actions@github.com']);
+		await execa('git', ['submodule', 'update', '--init', '--recursive']);
+	}
+});
+
 test.after.always(async () => {
 	await execa('git', ['submodule', 'update', '--remote']);
 });
 
 test('Integration tests', async t => {
-	await execa('ava', {cwd: 'integration-test'});
+	await execa('npx', ['ava'], {cwd: 'integration-test'});
 	t.pass();
 });
