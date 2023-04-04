b4349390d076ccb890a6ddb34adec918ed57bf10
tommy-mitchell <tmitchell7@uh.edu>
2023-04-04 10:26:41 -0500

    tests(`new-files`): add `.github` fixture

--- test/new-files.js
+++ test/new-files.js
@@ -16,49 +16,42 @@ const mockPkgDir = test.macro(async (t, fixture, expectedFiles, {before, after} 
 	});
 
 	const files = await npmUtil.getFilesToBePacked();
-	t.deepEqual(files.sort(), expectedFiles.sort(), 'Files different from expectations!');
+	t.deepEqual(files.sort(), [...expectedFiles, 'package.json'].sort(), 'Files different from expectations!');
 
 	await runIfExists(after, fixtureDir);
 });
 
 test('package.json files field - one file', mockPkgDir, 'one-file', [
-	'package.json',
 	'index.js',
 ]);
 
 test('package.json files field - source dir', mockPkgDir, 'source-dir', [
-	'package.json',
 	'source/foo.js',
 	'source/bar.js',
 ]);
 
 test('package.json files field - source and dist dirs', mockPkgDir, 'source-and-dist-dir', [
-	'package.json',
 	'source/foo.js',
 	'source/bar.js',
 ]);
 
 test('package.json files field - leading slash', mockPkgDir, 'files-slash', [
-	'package.json',
 	'index.js',
 ]);
 
 test('package.json files field - has readme and license', mockPkgDir, 'has-readme-and-license', [
-	'package.json',
 	'readme.md',
 	'license.md',
 	'index.js',
 ]);
 
 test('npmignore', mockPkgDir, 'npmignore', [
-	'package.json',
 	'readme.md',
 	'index.js',
 	'index.d.ts',
 ]);
 
 test('package.json files field and npmignore', mockPkgDir, 'files-and-npmignore', [
-	'package.json',
 	'readme.md',
 	'source/foo.js',
 	'source/bar.js',
@@ -75,20 +68,21 @@ const renameDotGitignore = {
 };
 
 test('package.json files field and gitignore', mockPkgDir, 'gitignore', [
-	'package.json',
 	'readme.md',
 	'dist/index.js',
 ], renameDotGitignore);
 
 test('npmignore and gitignore', mockPkgDir, 'npmignore-and-gitignore', [
-	'package.json',
 	'readme.md',
 	'dist/index.js',
 ], renameDotGitignore);
 
 test('package.json main field not in files field', mockPkgDir, 'main', [
-	'package.json',
 	'foo.js',
 	'bar.js',
+]);
+
+test('doesn\'t show files in .github', mockPkgDir, 'dot-github', [
+	'index.js',
 ]);
 
