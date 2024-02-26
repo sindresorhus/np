b54c77de766a3de2ba96fb70423152005604d4ee
Misha Kaletsky <15040698+mmkal@users.noreply.github.com>
2024-02-26 03:03:37 -0500

    Tag version prefix jsdoc

--- source/package-manager/types.d.ts
+++ source/package-manager/types.d.ts
@@ -41,7 +41,9 @@ export type PackageManagerConfig = {
   	*/
 	getRegistryCommand: Command;
 
-	/**  */
+	/**
+        CLI command expected to output the version tag prefix (often "v"). e,g. `['npm', ['config', 'get', 'tag-version-prefix']]`
+	*/
 	tagVersionPrefixCommand: Command;
 
 	/**
