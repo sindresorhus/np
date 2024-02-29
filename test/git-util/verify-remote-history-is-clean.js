bd36e077b2f55aa31013c94e8772e1103f0d82b5
tommy-mitchell <tmitchell7@uh.edu>
2024-02-29 15:47:12 -0600

    Update dependencies

--- test/git-util/verify-remote-history-is-clean.js
+++ test/git-util/verify-remote-history-is-clean.js
@@ -66,9 +66,7 @@ test('clean fetched remote history', createStubFixture, [
 test('no remote', createIntegrationFixture, async () => {
 	//
 }, async ({t, testedModule: {verifyRemoteHistoryIsClean}}) => {
-	const result = await t.notThrowsAsync(
+	await t.notThrowsAsync(
 		verifyRemoteHistoryIsClean(),
 	);
-
-	t.is(result, undefined);
 });
