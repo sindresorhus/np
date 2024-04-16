4f1aa37893f1f5b89e16b4f599a55b5a4e1bf7bb
Travis Fischer <fisch0920@gmail.com>
2024-04-15 20:17:41 -0500

    fix: npm 10 bug with npm pack; issue 740

--- source/npm/util.js
+++ source/npm/util.js
@@ -141,7 +141,8 @@ export const getFilesToBePacked = async rootDirectory => {
 	const {stdout} = await execa('npm', ['pack', '--dry-run', '--json', '--silent'], {cwd: rootDirectory});
 
 	try {
-		const {files} = JSON.parse(stdout).at(0);
+		const cleanStdout = stdout.replace(/^[^[]*\[/, '[').trim();
+		const {files} = JSON.parse(cleanStdout).at(0);
 		return files.map(file => file.path);
 	} catch (error) {
 		throw new Error('Failed to parse output of npm pack', {cause: error});
