a160affdc4c59b196e2c4d3445b13c219cb08687
Sindre Sorhus <sindresorhus@gmail.com>
2026-01-20 22:49:00 +0700

    Fix long release notes exceeding GitHub URL limit
    Fixes #501

--- source/release-task-helper.js
+++ source/release-task-helper.js
@@ -1,7 +1,12 @@
 import open from 'open';
 import newGithubReleaseUrl from 'new-github-release-url';
+import clipboard from 'clipboardy';
 import {getTagVersionPrefix, getPreReleasePrefix} from './util.js';
 import Version from './version.js';
+
+// GitHub has a URL limit of ~8195 characters. We use a conservative limit to be safe.
+const URL_LENGTH_LIMIT = 7900;
+const CLIPBOARD_PLACEHOLDER = '<!-- Paste release notes from clipboard -->';
 
 const releaseTaskHelper = async (options, package_, packageManager) => {
 	if (!options.repoUrl) {
@@ -14,12 +19,27 @@ const releaseTaskHelper = async (options, package_, packageManager) => {
 
 	const tag = await getTagVersionPrefix(packageManager) + newVersion.toString();
 
-	const url = newGithubReleaseUrl({
+	const releaseNotes = options.releaseNotes ? options.generateReleaseNotes(tag) : '';
+
+	// Try to generate URL with full release notes
+	let url = newGithubReleaseUrl({
 		repoUrl: options.repoUrl,
 		tag,
-		body: options.releaseNotes ? options.generateReleaseNotes(tag) : '',
+		body: releaseNotes,
 		isPrerelease: newVersion.isPrerelease(),
 	});
+
+	// If the URL is too long, copy release notes to clipboard and use a placeholder
+	if (url.length > URL_LENGTH_LIMIT) {
+		await clipboard.write(releaseNotes);
+		url = newGithubReleaseUrl({
+			repoUrl: options.repoUrl,
+			tag,
+			body: CLIPBOARD_PLACEHOLDER,
+			isPrerelease: newVersion.isPrerelease(),
+		});
+		console.log('\nRelease notes are too long for URL. Copied to clipboard instead.');
+	}
 
 	await open(url);
 };
--- test/release-task-helper.js
+++ test/release-task-helper.js
@@ -8,6 +8,9 @@ const verifyRelease = test.macro(async (t, {oldVersion, newVersion, prefixes = {
 	/** @type {import('../source/release-task-helper.js')} */
 	const {default: releaseTaskHelper} = await esmock('../source/release-task-helper.js', import.meta.url, {
 		open: sinon.stub(),
+		clipboardy: {
+			write: sinon.stub(),
+		},
 		'../source/util.js': {
 			getTagVersionPrefix: async () => prefixes.tag ?? 'v',
 			getPreReleasePrefix: async () => prefixes.preRelease ?? '',
@@ -63,4 +66,109 @@ test('prerelease', verifyRelease, {
 		tag: 'v1.0.1-beta.0',
 		isPrerelease: true,
 	},
+});
+
+test('uses clipboard when URL is too long', async t => {
+	const repoUrl = 'https://github.com/sindresorhus/np';
+	const clipboardStub = sinon.stub();
+	const openStub = sinon.stub();
+	const urlCalls = [];
+
+	// Generate a very long release notes string that will exceed the URL limit
+	const longReleaseNotes = 'x'.repeat(8000);
+
+	const {default: releaseTaskHelper} = await esmock('../source/release-task-helper.js', import.meta.url, {
+		open: openStub,
+		clipboardy: {
+			write: clipboardStub,
+		},
+		'../source/util.js': {
+			async getTagVersionPrefix() {
+				return 'v';
+			},
+			async getPreReleasePrefix() {
+				return '';
+			},
+		},
+		'new-github-release-url'(options_) {
+			urlCalls.push(options_);
+			// Generate a realistic URL
+			const baseUrl = `${options_.repoUrl}/releases/new`;
+			const parameters = new URLSearchParams({
+				tag: options_.tag,
+				body: options_.body,
+				prerelease: options_.isPrerelease ? '1' : '0',
+			});
+			return `${baseUrl}?${parameters.toString()}`;
+		},
+	});
+
+	await releaseTaskHelper(
+		{
+			version: '1.1.0',
+			repoUrl,
+			releaseNotes: true,
+			generateReleaseNotes: () => longReleaseNotes,
+		},
+		{version: '1.0.0'},
+	);
+
+	// Should be called twice: once with long notes, once with placeholder
+	t.is(urlCalls.length, 2);
+	t.is(urlCalls[0].body, longReleaseNotes);
+	t.is(urlCalls[1].body, '<!-- Paste release notes from clipboard -->');
+	t.true(clipboardStub.calledOnce);
+	t.true(clipboardStub.calledWith(longReleaseNotes));
+	t.true(openStub.calledOnce);
+});
+
+test('does not use clipboard when URL is short enough', async t => {
+	const repoUrl = 'https://github.com/sindresorhus/np';
+	const clipboardStub = sinon.stub();
+	const openStub = sinon.stub();
+	const urlCalls = [];
+
+	const shortReleaseNotes = 'Short release notes';
+
+	const {default: releaseTaskHelper} = await esmock('../source/release-task-helper.js', import.meta.url, {
+		open: openStub,
+		clipboardy: {
+			write: clipboardStub,
+		},
+		'../source/util.js': {
+			async getTagVersionPrefix() {
+				return 'v';
+			},
+			async getPreReleasePrefix() {
+				return '';
+			},
+		},
+		'new-github-release-url'(options_) {
+			urlCalls.push(options_);
+			// Generate a realistic URL
+			const baseUrl = `${options_.repoUrl}/releases/new`;
+			const parameters = new URLSearchParams({
+				tag: options_.tag,
+				body: options_.body,
+				prerelease: options_.isPrerelease ? '1' : '0',
+			});
+			return `${baseUrl}?${parameters.toString()}`;
+		},
+	});
+
+	await releaseTaskHelper(
+		{
+			version: '1.1.0',
+			repoUrl,
+			releaseNotes: true,
+			generateReleaseNotes: () => shortReleaseNotes,
+		},
+		{version: '1.0.0'},
+	);
+
+	// Should be called only once with the short notes
+	t.is(urlCalls.length, 1);
+	t.is(urlCalls[0].body, shortReleaseNotes);
+	t.false(clipboardStub.called);
+	t.true(openStub.calledOnce);
 });
