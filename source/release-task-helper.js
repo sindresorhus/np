dc62a24830d6324f1513c4cba06e5936951e5a77
Sindre Sorhus <sindresorhus@gmail.com>
2026-01-20 21:19:53 +0700

    Add clear error message when `repository` field is missing
    Fixes #639

--- source/release-task-helper.js
+++ source/release-task-helper.js
@@ -4,6 +4,10 @@ import {getTagVersionPrefix, getPreReleasePrefix} from './util.js';
 import Version from './version.js';
 
 const releaseTaskHelper = async (options, package_, packageManager) => {
+	if (!options.repoUrl) {
+		throw new Error('Missing `repository` field in package.json. This is required for creating GitHub releases.');
+	}
+
 	const newVersion = options.releaseDraftOnly
 		? new Version(package_.version)
 		: new Version(package_.version).setFrom(options.version.toString(), {prereleasePrefix: await getPreReleasePrefix(packageManager)});
